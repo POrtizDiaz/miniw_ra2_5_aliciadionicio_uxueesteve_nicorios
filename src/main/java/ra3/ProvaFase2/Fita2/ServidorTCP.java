43a280940e79d9db2b5b4363e3b3378b3bbab62a
POrtizDiaz <peordi24@bemen3.cat>
2025-12-18 16:07:34 +0100

    Codi actualitzat amb la fase 2

--- src/main/java/ra3/ProvaFase2/Fita2/ServidorTCP.java
+++ src/main/java/ra3/ProvaFase2/Fita2/ServidorTCP.java
@@ -1,7 +1,11 @@
 package ra3.ProvaFase2.Fita2;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServidorTCP {
     public static void main(String[] args) {
